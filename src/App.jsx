751cba3518cfd486ae8c5374add8915a3f9c0845
AdnanAhmed04 <adnanahmedb7208@gmail.com>
2025-07-08 03:42:05 +0500

    deployment issues

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,37 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Sidebar from './components/Sidebar';
-import HomePage from './pages/HomePage';
-import SearchPage from './pages/SearchPage';
-import VideoDetailPage from './pages/VideoDetailPage';
-import Signup from './pages/signup';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-function App() {
+// Components
+import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
+
+// Pages
+import HomePage from "./pages/HomePage";
+import CategoryPage from "./pages/CategoryPage";
+import SearchPage from "./pages/SearchPage";
+import VideoDetailPage from "./pages/VideoDetailPage";
+import Signup from "./pages/Signup"; // ✅ Correct casing
+
+const App = () => {
   return (
-    <BrowserRouter>
-      <Navbar />
-      <div className="flex">
-        <Sidebar />
-        <main className="flex-1 p-6 bg-zinc-100 min-h-screen">
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/search/:query" element={<SearchPage />} />
-            <Route path="/video/:id" element={<VideoDetailPage />} />
-                        <Route path="/Signup" element={<Signup />} />
-
-          </Routes>
-        </main>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="flex">
+          <Sidebar />
+          <div className="flex-1 p-4">
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/category/:categoryId" element={<CategoryPage />} />
+              <Route path="/search/:query" element={<SearchPage />} />
+              <Route path="/video/:videoId" element={<VideoDetailPage />} />
+              <Route path="/signup" element={<Signup />} />
+            </Routes>
+          </div>
+        </div>
       </div>
-    </BrowserRouter>
+    </Router>
   );
-}
+};
 
 export default App;
